0b135d66c968fca2d9f67a1ba072a23ac0e7d8a8
asyfa <muutukung@gmail.com>
2016-04-22 14:23:02 +0700

    Add code

---
+++ src/kalkulator/Kalkulator.java
@@ -0,0 +1,12 @@
+
+package kalkulator;
+
+public class Kalkulator {
+    
+    public static void main(String[] args) {
+        FrmKalkulator calc;
+        calc = new FrmKalkulator();
+        calc.show();
+    }
+    
+}
